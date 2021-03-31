521cf8a805f0f6b6cc86d03e9388368c3bab62ad
DESKTOP-J67BSCC\DELL <kietnguyen1147@gmail.com>
2021-03-31 16:39:19 +0700

    update menu/ perfect pixel

--- src/assets/javascripts/modules/ModAboutContent.js
+++ src/assets/javascripts/modules/ModAboutContent.js
@@ -1,8 +1,8 @@
 const $this = $('.mod-content-about')
 
 const init = () => {
-  $('.row_p').hide()
-  $('.row-p1').show()
+  // $('.row_p').hide()
+  // $('.row-p1').show()
 }
 
 const bindEvent = () => {
@@ -10,9 +10,31 @@ const bindEvent = () => {
     const targetClass = $(this).data('target')
     const target = $(`.${targetClass}`)
     $(this).toggleClass('active').siblings().removeClass('active')
-    target.show()
-    $('.row_p').not(target).hide()
+    $('.about-main ').removeClass('is-show')
+    target.addClass('is-show')
+    // target.show()
+    // $('.row_p').not(target).hide()
   })
+  $('.show-mb').on('click', (e) => {
+    const parent = $(e.currentTarget).parents('.about-main ')
+    const content = parent.find('.about-slide')
+    if (parent.hasClass('is-show')) {
+      parent.removeClass('is-show')
+      content.slideUp()
+    } else {
+      $('.about-slide ').slideUp().removeClass('is-show')
+      parent.addClass('is-show')
+      content.slideDown()
+    }
+  })
+
+  // $('.show_mb').on('click', function () {
+  //   $(this).toggleClass('active').siblings().removeClass('active')
+  //   const row = $(this).children('.row-p-mb')
+  //   console.log(row)
+  //   $('.row-p-mb').not(row).slideUp()
+  //   row.slideToggle('fast')
+  // })
 }
 
 const ModAboutContent = (() => {
