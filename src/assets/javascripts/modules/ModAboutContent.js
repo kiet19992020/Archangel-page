5414e0a3026c57255fd70609d9a4ec2a1f1b3995
DESKTOP-J67BSCC\DELL <kietnguyen1147@gmail.com>
2021-04-01 16:34:13 +0700

    update perfect pixel

--- src/assets/javascripts/modules/ModAboutContent.js
+++ src/assets/javascripts/modules/ModAboutContent.js
@@ -1,8 +1,7 @@
 const $this = $('.mod-content-about')
 
 const init = () => {
-  // $('.row_p').hide()
-  // $('.row-p1').show()
+  $this.find('.is-show .about-slide').show()
 }
 
 const bindEvent = () => {
@@ -12,29 +11,22 @@ const bindEvent = () => {
     $(this).toggleClass('active').siblings().removeClass('active')
     $('.about-main ').removeClass('is-show')
     target.addClass('is-show')
-    // target.show()
-    // $('.row_p').not(target).hide()
   })
+
   $('.show-mb').on('click', (e) => {
     const parent = $(e.currentTarget).parents('.about-main ')
     const content = parent.find('.about-slide')
+
     if (parent.hasClass('is-show')) {
       parent.removeClass('is-show')
       content.slideUp()
     } else {
-      $('.about-slide ').slideUp().removeClass('is-show')
-      parent.addClass('is-show')
-      content.slideDown()
+      const prArr = $('.about-main').not(parent)
+      prArr.find('.about-slide').slideUp()
+      parent.toggleClass('is-show').siblings().removeClass('is-show')
+      content.slideToggle('fast')
     }
   })
-
-  // $('.show_mb').on('click', function () {
-  //   $(this).toggleClass('active').siblings().removeClass('active')
-  //   const row = $(this).children('.row-p-mb')
-  //   console.log(row)
-  //   $('.row-p-mb').not(row).slideUp()
-  //   row.slideToggle('fast')
-  // })
 }
 
 const ModAboutContent = (() => {
---
+++ src/html/data/modules/protect.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    title: 'Biofuels',
+    icon: '../assets/images/Aquaculture-Icon3.png',
+    img: '/assets/images/Archangel-Biofuel.jpg'
+  },
+  {
+    title: 'Beverage alcohol',
+    icon: '../assets/images/Aquaculture-Icon3.png',
+    img: '/assets/images/Archangel-Biofuel.jpg'
+  },
+  {
+    title: 'Aquaculture',
+    icon: '../assets/images/Aquaculture-Icon3.png',
+    img: '/assets/images/Archangel-Biofuel.jpg'
+  },
+  {
+    title: 'Agriculture',
+    icon: '../assets/images/Aquaculture-Icon3.png',
+    img: '/assets/images/Archangel-Biofuel.jpg'
+  }
+]
